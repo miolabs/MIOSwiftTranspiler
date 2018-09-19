4ae70fac41ab7416c7c1fb54a3dd6fc2579c5349
Marcel Ganczak <marcelganczak@gmail.com>
2018-09-19 15:29:22 +0200

    generics WIP; functions - parsing & inferring; test for class method that uses class's generic

--- example.swift
+++ example.swift
@@ -1,43 +1,24 @@
-prefix operator +++
-infix operator +-: AdditionPrecedence
-struct Vector2D {
-    var x = 0, y = 0
-    static func + (left: Vector2D, right: Vector2D) -> Vector2D {
-        return Vector2D(x: left.x + right.x, y: left.y + right.y)
-    }
-    static prefix func - (vector: Vector2D) -> Vector2D {
-        return Vector2D(x: -vector.x, y: -vector.y)
-    }
-    static func += (left: inout Vector2D, right: Vector2D) {
-        left = left + right
-    }
-    static prefix func +++ (vector: inout Vector2D) -> Vector2D {
-        vector += vector
-        return vector
-    }
-    static func +- (left: Vector2D, right: Vector2D) -> Vector2D {
-        return Vector2D(x: left.x + right.x, y: left.y - right.y)
+func swapTwoValues<T>(_ a: inout T, _ b: inout T) {
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
+
+var someInt = 3
+var anotherInt = 107
+swapTwoValues(&someInt, &anotherInt)
+print(someInt)
+print(anotherInt)
+
+func justReturn<T>(_ a: T) -> T {
+    return a
+}
+print(justReturn("elo").characters.count)
+
+class Elo<T> {
+    func justReturn(_ a: T) -> T {
+        return a
     }
 }
-let vector = Vector2D(x: 3, y: 1)
-let anotherVector = Vector2D(x: 2, y: 4)
-let combinedVector = vector + anotherVector
-print(combinedVector.x)
-print(combinedVector.y)
-let inverseCombinedVector = -combinedVector
-print(inverseCombinedVector.x)
-print(inverseCombinedVector.y)
-var original = Vector2D(x: 1, y: 2)
-let vectorToAdd = Vector2D(x: 3, y: 4)
-original += vectorToAdd
-print(original.x)
-print(original.y)
-var toBeDoubled = Vector2D(x: 1, y: 4)
-let afterDoubling = +++toBeDoubled
-print(afterDoubling.x)
-print(afterDoubling.y)
-let firstVector = Vector2D(x: 1, y: 2)
-let secondVector = Vector2D(x: 3, y: 4)
-let plusMinusVector = firstVector +- secondVector
-print(plusMinusVector.x)
-print(plusMinusVector.y)
+var elo = Elo<String>()
+print(elo.justReturn("elo").characters.count)
--- test/generics/generic-function.swift
+++ test/generics/generic-function.swift
@@ -14,3 +14,11 @@ func justReturn<T>(_ a: T) -> T {
     return a
 }
 print(justReturn("elo").characters.count)
+
+class Elo<T> {
+    func justReturn(_ a: T) -> T {
+        return a
+    }
+}
+var elo = Elo<String>()
+print(elo.justReturn("elo").characters.count)
