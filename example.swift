7351e35935ba6896b3aabb1dc44a40f6c657876a
Marcel Ganczak <marcelganczak@gmail.com>
2019-01-06 02:44:33 +0100

    adjustment for copying structs on assignment

--- example.swift
+++ example.swift
@@ -1,6 +1,31 @@
-struct Point {
-    var x = 0.0, y = 0.0
-    mutating func moveBy(x deltaX: Double, y deltaY: Double) {
-        self = Point(x: x + deltaX, y: y + deltaY)
-    }
+struct PreStruct {
+  var prop = 0
 }
+class Class {
+  var prop = 0
+  var subStruct = PreStruct()
+}
+struct Struct {
+  var prop = 0
+  var subStruct = PreStruct()
+  var subClass = Class()
+}
+var s = Struct()
+var s2 = s
+s2.prop = 1
+s2.subStruct.prop = 1
+s2.subClass.prop = 1
+s2.subClass.subStruct.prop = 1
+print(s.prop)
+print(s2.prop)
+print(s.subStruct.prop)
+print(s2.subStruct.prop)
+print(s.subClass.prop)
+print(s2.subClass.prop)
+print(s.subClass.subStruct.prop)
+print(s2.subClass.subStruct.prop)
+var c = Class()
+var c2 = c
+c2.prop = 1
+print(c.prop)
+print(c2.prop)
--- test/underscore.js
+++ test/underscore.js
@@ -1,5 +1,13 @@
 var _ = {
     size: function(obj) { return Object.keys(obj).length },
-    clone: function(obj) { return obj instanceof Array ? obj.slice() : Object.assign({}, obj) },
+    cloneStruct: function(obj) {
+        if(!(obj instanceof Object) || !obj.$struct) return obj
+        var cloned = new obj.constructor()
+        for(var prop in obj) {
+            if(!obj.hasOwnProperty(prop)) continue
+            cloned[prop] = _.clone(obj[prop])
+        }
+        return cloned
+    },
     failableInit: function(obj) { return obj.$failed ? null : obj }
 };
