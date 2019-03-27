449e98b6fd3cc20f44e8fbdb4049152daf3b39f9
Marcel Ganczak <marcelganczak@gmail.com>
2019-03-27 20:00:00 +0100

    uncommented test for extending native protocols

--- test/local/classes/extensions-native.swift
+++ test/local/classes/extensions-native.swift
@@ -8,15 +8,12 @@ extension Array {
     print("ok array")
   }
 }
-/*
-not allowed for now
 extension Collection {
   func printOk2() {
     print("ok collection")
   }
 }
-*/
 
 (1..<5).printOk()
 ["str"].printOk()
-//["str"].printOk2()
+["str"].printOk2()
