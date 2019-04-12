b98cd946cc6c7531091b270b45e68c1d5392e878
Marcel Ganczak <marcelganczak@gmail.com>
2019-04-12 03:14:05 +0200

    automated refactor of miojslibs: build.js / readme TODO - testing!

--- template-build-miojs.js
+++ template-build-miojs.js
@@ -3,5 +3,14 @@
 const execSync = require('child_process').execSync
 const fs = require('fs')
 
-execSync(`cd ${__dirname}/MIOJSLibs && npm run dev && cd ..`)
-fs.copyFileSync(`${__dirname}/MIOJSLibs/packages/miojslibs/dist/js/miojslibs.js`, `${__dirname}/MIOJSLibs/templates/project/app/libs/miojslibs/miojslibs.js`)
+execSync(`cd ${__dirname}/MIOJSLibs && export TRANSPILER=false && npm run dev && cd ..`)
+
+execSync(`node ${__dirname}/include/build-bodies/build-miojslibs.js`)
+execSync(`ts-node --disableWarnings ${__dirname}/include/build-bodies/refactor-miojslibs.ts`)
+
+execSync(`cd ${__dirname}/MIOJSLibs && export TRANSPILER=true && npm run dev && cd ..`)
+
+fs.copyFileSync(
+    `${__dirname}/packages/miojslibs-swift-transpiler/build/miojslibs-swift-transpiler.js`,
+    `${__dirname}/MIOJSLibs/templates/project/app/libs/miojslibs/miojslibs.js`
+)
